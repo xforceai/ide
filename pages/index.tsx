2620622b3124a35124955bf93f92f1c3a7060653
cemtu <cem_tuncelli@protonmail.ch>
2024-01-12 10:05:38 -0800

    disclaimer

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,22 @@ const IDE = () => {
     e.preventDefault();
   };
   return (
-    <div onContextMenu={disableDefaultContextMenu}>
-      <ValidatorContextProvider>
-        <ModalContextProvider>
-          <ReactFlowProvider>
-            <AppX />
-          </ReactFlowProvider>
-        </ModalContextProvider>
-      </ValidatorContextProvider>
-    </div>
+    <>
+      <div className="block xl:hidden lg:hidden md:hidden whitespace-pre-line">
+        X-Force IDE is a low code agent as a service UI framework that lets you to create agent based workforces from
+        drag and drop like user interface.{'\n\n'} In order to use drag and drop features and code execution ability,
+        you need to access the IDE using your computer.
+      </div>
+      <div onContextMenu={disableDefaultContextMenu} className="hidden xl:block lg:block md:block">
+        <ValidatorContextProvider>
+          <ModalContextProvider>
+            <ReactFlowProvider>
+              <AppX />
+            </ReactFlowProvider>
+          </ModalContextProvider>
+        </ValidatorContextProvider>
+      </div>
+    </>
   );
 };
 
